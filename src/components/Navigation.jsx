a67a16dc0e54d7512d762e36973d149b6dfe54e6
Leo Koskimäki <103430948+Lekematti@users.noreply.github.com>
2024-04-09 11:24:59 +0300

    path & page for workpalces

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,7 @@ import Register from "./Register.jsx";
 import Home from "./Home.jsx";
 import {MainContext} from "../Context/MainContext.jsx";
 import {useContext} from "react";
+import WorkPlaces from "./WorkPlaces.jsx";
 
 function Navigation() {
     const {isLogged} = useContext(MainContext);
@@ -11,11 +12,12 @@ function Navigation() {
         isLogged ? (
             <Routes>
                 <Route path="/home" element={<Home/>}/>
+                <Route path="/manage-work-places" element={<WorkPlaces/>}/>
                 {/*<Route path="/add-roles" element={<AddRoles/>}/>
                 <Route path="/manage-roles" element={<ManageRoles/>}/>
                 <Route path="/manage-users" element={<ManageUsers/>}/>
                 <Route path="/manage-work-hours" element={<ManageWorkHours/>}/>
-                <Route path="/manage-work-places" element={<ManageWorkPlaces/>}/>*/}
+                */}
             </Routes>
         ) : (
             <Routes>
---
+++ src/components/WorkPlaces.jsx
@@ -0,0 +1,16 @@
+import Sidebar from './Sidebar';
+import '../CSS/Home.css';
+
+function WorkPlaces() {
+    return (
+        <div className="home">
+            <Sidebar/>
+            <div className="main-content">
+                <h1>Work palace management screen!</h1>
+                {/* ... other code ... */}
+            </div>
+        </div>
+    );
+}
+
+export default WorkPlaces;
