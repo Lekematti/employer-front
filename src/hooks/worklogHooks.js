0837d5a3832ef07483d709599263e3e0c78a0932
Kim Rautiainen <kim.rautiainen@hotmail.com>
2024-04-09 16:33:52 +0300

    worklog hooks

---
+++ src/hooks/worklogHooks.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+const API_URL = "http://localhost:3000/";
+
+const worklogHooks = () => {
+  // get token from session storage
+  const tokenStr = sessionStorage.getItem("token");
+  const token = JSON.parse(tokenStr);
+
+  // get all worklogs for companyId
+  const getWorkLogsByCompanyId = async (companyId) => {
+    try {
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const res = await axios.get(
+        API_URL + "workLogs/company/" + companyId,
+        options
+      );
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // get all worklogs
+  const getWorkLogs = async () => {
+    try {
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const res = await axios.get(API_URL + "workLogs/", options);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // get worklog by userId
+  const getWorkLogByUserId = async (userId) => {
+    try {
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const res = await axios.get(API_URL + "workLogs/" + userId, options);
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  // get worklog by userId for workAreaId
+  const getWorkLogByUserIdAndWorkAreaId = async (userId, workAreaId) => {
+    try {
+      const options = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const res = await axios.get(
+        API_URL + "workLogs/" + userId + "/" + workAreaId,
+        options
+      );
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return {
+    getWorkLogsByCompanyId,
+    getWorkLogs,
+    getWorkLogByUserId,
+    getWorkLogByUserIdAndWorkAreaId,
+  };
+};
+
+export default worklogHooks;
